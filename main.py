ef3b65ce46262c28bd53a98169e375aa5b859da6
Corban Bell <121130851+Corbanizer@users.noreply.github.com>
2023-12-15 08:49:50 -0600

    Update 1.2

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ x1= f(i('Enter (x1) --> '))
 y1= f(i('Enter (y1) --> '))
 x2= f(i('Enter (x2) --> '))
 y2= f(i('Enter (y2) --> '))
-diff_x= x2 - x1
-diff_y= y2 - y1
-p('Slope =',diff_y,'/',diff_x)
-p('Slope =',diff_y / diff_x)
-in_root = diff_x ** 2 + diff_y ** 2
+dif_x= x2 - x1
+dif_y= y2 - y1
+p('Slope =',dif_y,'/',dif_x)
+p('Slope =',dif_y / dif_x)
+in_root = dif_x ** 2 + dif_y ** 2
 if in_root.is_integer():
-    coefficient = 1           
+    coef = 1           
     root_num = in_root 
     for i in range(int(in_root), 0, -1): 
-        coefficient = math.sqrt(i)
-        if coefficient.is_integer() and in_root % i == 0:
+        coef = math.sqrt(i)
+        if coef.is_integer() and in_root % i == 0:
                 root_num = in_root / i
                 break
-    if coefficient != 1:
-        p('Distance = sqrt of', in_root, '=', coefficient, 'sqrt', root_num)
+    if coef != 1:
+        p('Distance = sqrt of', in_root, '=', coef, 'sqrt', root_num)
     else:
         p('Distance = sqrt of', in_root)
