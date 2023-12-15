a73e72ec9fabe5decb4ba644f990ee621112ddc4
Corban Bell <121130851+Corbanizer@users.noreply.github.com>
2023-12-15 09:51:30 -0600

    Update 1.5

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ x1= f(i('Enter (x1) --> '))
 y1= f(i('Enter (y1) --> '))
 x2= f(i('Enter (x2) --> '))
 y2= f(i('Enter (y2) --> '))
-dif_x= x2 - x1
-dif_y= y2 - y1
+dif_x= x2-x1
+dif_y= y2-y1
 p('Slope =',dif_y,'/',dif_x)
-p('Slope =',dif_y / dif_x)
+p('Slope =',dif_y/dif_x)
 in_r = dif_x ** 2 + dif_y ** 2
 if in_r.is_integer():
     coef = 1           
-    root_num = in_r 
-    for i in range(int(in_root), 0, -1): 
+    r_num = in_r 
+    for i in range(int(in_r), 0, -1): 
         coef = math.sqrt(i)
         if coef.is_integer() and in_r % i == 0:
-                root_num = in_r / i
+                r_num = in_r / i
                 break
     if coef != 1:
-        p('Distance = sqrt of', in_r, '=', coef, 'sqrt', root_num)
+        p('Distance = sqrt of', in_r, '=', coef, 'sqrt', r_num)
     else:
         p('Distance = sqrt of', in_r)
