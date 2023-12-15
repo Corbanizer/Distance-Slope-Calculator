8c218447a218933dbbb1400099c3c2f64171592b
Corban Bell <121130851+Corbanizer@users.noreply.github.com>
2023-12-15 08:47:44 -0600

    Update 1.1

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ diff_x= x2 - x1
 diff_y= y2 - y1
 p('Slope =',diff_y,'/',diff_x)
 p('Slope =',diff_y / diff_x)
-inside_root = diff_x ** 2 + diff_y ** 2
-if inside_root.is_integer():
+in_root = diff_x ** 2 + diff_y ** 2
+if in_root.is_integer():
     coefficient = 1           
-    root_num = inside_root 
-    for i in range(int(inside_root), 0, -1): 
+    root_num = in_root 
+    for i in range(int(in_root), 0, -1): 
         coefficient = math.sqrt(i)
-        if coefficient.is_integer() and inside_root % i == 0:
-                root_num = inside_root / i
+        if coefficient.is_integer() and in_root % i == 0:
+                root_num = in_root / i
                 break
     if coefficient != 1:
-        p('Distance = sqrt of', inside_root, '=', coefficient, 'sqrt', root_num)
+        p('Distance = sqrt of', in_root, '=', coefficient, 'sqrt', root_num)
     else:
-        p('Distance = sqrt of', inside_root)
+        p('Distance = sqrt of', in_root)
