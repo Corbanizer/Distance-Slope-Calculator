f89d70f6ee344f27892cc320eafd027e3c92fdaf
Corban Bell <121130851+Corbanizer@users.noreply.github.com>
2023-12-21 10:25:00 -0600

    v3.0

--- main.py
+++ main.py
@@ -1,36 +1,53 @@
-import math
+import math as m
 import time as t
 
 p = print
-p(":3")
+p(':3')
 f = float
 i = input
-x1 = f(i('Enter (x1) --> '))
-y1 = f(i('Enter (y1) --> '))
-x2 = f(i('Enter (x2) --> '))
-y2 = f(i('Enter (y2) --> '))
-p(" ")
-s = t.time()
+b = 'Enter ('
+a = ') --> '
+x1 = f(i(b + 'x1' + a))
+y1 = f(i(b + 'y1' + a))
+x2 = f(i(b + 'x2' + a))
+y2 = f(i(b + 'y2' + a))
+del a, b, f, i
+p('')
+s = 'Slope ='
+st = t.time()
 dif_x = x2 - x1
 dif_y = y2 - y1
-_s = 'Slope ='
-p(_s, dif_y, '/', dif_x)
-p(_s, dif_y / dif_x)
-p(" ")
+del y1, y2, x1, x2
+p(s, dif_y, '/', dif_x)
+if dif_x == 0 or dif_y == 0:
+    p('You cant divide by 0')
+    exit()
+p(s, dif_y / dif_x)
+del s
+p('')
 in_r = dif_x**2 + dif_y**2
-_d = 'Distance = sqrt of'
+d = 'Distance = sqrt of'
 if in_r.is_integer():
-  coef = 1
-  r_num = in_r
-  for i in range(int(in_r), 0, -1):
-    coef = math.sqrt(i)
-    if coef.is_integer() and in_r % i == 0:
-      r_num = in_r / i
-      break
-  if coef != 1:
-    p(_d, in_r, '=', coef, 'sqrt', r_num)
-  else:
-    p(_d, in_r)
+    coef = 1
+    r_num = in_r
+    n = m.sqrt
+    for i in range(int(in_r), 0, -1):
+        coef = n(i)
+        if coef.is_integer() and in_r % i == 0:
+            r_num = in_r / i
+            break
+    if coef != 1:
+        e = t.time()
+        p(d, in_r, '=', coef, 'sqrt', r_num)
+        del r_num, in_r, d, n
+    else:
+        e = t.time()
+        p(d, in_r)
+        del r_num, in_r, d, n
 e = t.time()
-p(" ")
-p("Time to calculate =",e-s,"seconds")
+p('')
+if e - st < 0.0001:
+    p('Too fast to display time')
+else:
+    p('Time to calculate =', e - st, 'seconds')
+exit()
